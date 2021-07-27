63919cb0fb5e5bf8164f8ed5ffa0c86a23c29a2a
ManoCodigo <lucascostabrandao@gmail.com>
2021-07-27 08:21:02 -0300

    Lista de Cargos

---
+++ exemplos/src/app/listar-cargos/listar-cargos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-cargos',
+  templateUrl: './listar-cargos.component.html',
+  styleUrls: ['./listar-cargos.component.css']
+})
+export class ListarCargosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
